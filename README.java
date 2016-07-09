7f5d478832ceace1b4e552d2934dbe1a1e3656be
prakashpks <prakars878740@gmail.com>
2016-07-09 10:10:24 +0530

    finish

---
+++ README.java
@@ -0,0 +1,13 @@
+# swap
+package k;
+public class b {
+public static void main(String []args)
+{
+int a=13,num,b=12;
+System.out.printf("before swap a=%d b=%d",a,b);
+num=a;
+a=b;
+b=num;
+System.out.printf("\nafter swap a=%d b=%d",a,b);
+}
+}
